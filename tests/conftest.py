e5893c27ba9c09d50cdf33d6a5b2d11683a3c151
Alex Franz <alexfranz90@googlemail.com>
2024-05-28 09:20:38 +0200

    update poetry lock

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,4 @@
 import os
 import sys
-from pytest_socket import disable_socket
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-
-def pytest_runtest_setup():
-    disable_socket()
