6ab2fe7be45071a98a955b6e1c313e183d60569f
Alex Franz <alexfranz90@googlemail.com>
2024-05-30 12:50:13 +0200

    add tests

--- garminworkouts/config/configreader.py
+++ garminworkouts/config/configreader.py
@@ -2,7 +2,14 @@ import yaml
 from garminworkouts.config.includeloader import IncludeLoader
 from typing import Union
 from garminworkouts.models.workout import Workout
-from garminworkouts.models.running_workout import RunningWorkout
+from garminworkouts.models.running_workout import (
+    RunningWorkout,
+    RunningWorkoutConfig,
+    WorkoutStep,
+)
+from garminworkouts.models.duration import Duration
+
+from garminworkouts.models.pace import PaceRange
 from pathlib import Path
 
 
@@ -13,7 +20,35 @@ def read_config(filename: Union[str, Path]) -> dict:
 
 
 def parse_config(config: dict) -> Union[Workout, RunningWorkout]:
-    if config["type"] == "running":
-        return RunningWorkout(config)
+    if config["settings"]["sports_type"] == "running":
+        zones = [
+            PaceRange.from_str(z_name, zone_str)
+            for z_name, zone_str in config["settings"]["zones"].items()
+        ]
+        workout_steps = []
+        for step in config["steps"]:
+            duration = step.get("duration")
+            if duration:
+                duration = Duration(duration).to_seconds()
+            distance = step.get("distance")
+            if distance:
+                # TODO: Implement distance parser conversion
+                distance = distance
+
+            workout_steps.append(
+                WorkoutStep(
+                    duration=duration,
+                    distance=distance,
+                    target=step["target"],
+                )
+            )
+
+        run_config = RunningWorkoutConfig(
+            name=config["name"],
+            description=config["description"],
+            zones=zones,
+            steps=workout_steps,
+        )
+        return RunningWorkout(run_config)
     else:
         return Workout(config)
--- tests/config/test_configreader.py
+++ tests/config/test_configreader.py
@@ -1,14 +1,19 @@
 import os
 
-from garminworkouts.config import configreader
+from garminworkouts.config.configreader import read_config, parse_config
 
 
-def test_configreader():
-    config_file = os.path.join(os.path.dirname(__file__), "test_configreader.yaml")
-    config = configreader.read_config(config_file)
+def get_test_file(filename: str):
+    config_file = os.path.join(os.path.dirname(__file__), "workouts", filename)
+    return config_file
+
+
+def test_configreader_cycling():
+    config_file = get_test_file("cycling/test_configreader.yaml")
+    config = read_config(config_file)
 
     expected_config = {
-        "name": "Test",
+        "name": "Cycling Test",
         "settings": {
             "sports_type": "cycling",
             "ftp": 250,
@@ -21,3 +26,30 @@ def test_configreader():
         ],
     }
     assert config == expected_config
+
+
+def test_configreader_running():
+    config_file = get_test_file("running/test_configreader.yaml")
+    config = read_config(config_file)
+
+    expected_config = {
+        "name": "Running 30 Min",
+        "description": "5 min easy, 20 min tempo, 5 min rest",
+        "settings": {
+            "sports_type": "running",
+            "zones": {"easy": "5:00-5:25", "tempo": "4:10-4:20", "rest": "5:30-10:00"},
+        },
+        "steps": [
+            {"target": "easy", "duration": "5:00"},
+            {"target": "tempo", "duration": "20:00"},
+            {"target": "rest", "duration": "5:00"},
+        ],
+    }
+    assert config == expected_config
+
+
+def test_parse_config_running():
+    config_file = get_test_file("running/test_configreader.yaml")
+    config = read_config(config_file)
+    workout = parse_config(config)
+    assert workout.__class__.__name__ == "RunningWorkout"
