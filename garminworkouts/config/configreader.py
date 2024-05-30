63cf91b1d8972496b65abf399ce315e2f4a0d6c4
Alex Franz <alexfranz90@googlemail.com>
2024-05-30 08:07:00 +0200

    add typer

--- garminworkouts/config/configreader.py
+++ garminworkouts/config/configreader.py
@@ -1,8 +1,19 @@
 import yaml
 from garminworkouts.config.includeloader import IncludeLoader
+from typing import Union
+from garminworkouts.models.workout import Workout
+from garminworkouts.models.running_workout import RunningWorkout
+from pathlib import Path
 
 
-def read_config(filename):
+def read_config(filename: Union[str, Path]) -> dict:
     with open(filename, "r") as f:
         data = yaml.load(f, IncludeLoader)
     return data
+
+
+def parse_config(config: dict) -> Union[Workout, RunningWorkout]:
+    if config["type"] == "running":
+        return RunningWorkout(config)
+    else:
+        return Workout(config)
--- tests/config/test_configreader.py
+++ tests/config/test_configreader.py
@@ -1,26 +1,23 @@
 import os
-import unittest
 
 from garminworkouts.config import configreader
 
 
-class MyTestCase(unittest.TestCase):
-    def test_read_config(self):
-        config_file = os.path.join(os.path.dirname(__file__), "test_configreader.yaml")
-        config = configreader.read_config(config_file)
+def test_configreader():
+    config_file = os.path.join(os.path.dirname(__file__), "test_configreader.yaml")
+    config = configreader.read_config(config_file)
 
-        expected_config = {
-            "name": "Test",
-            "steps": [
-                [{"power": 50, "duration": "2:00"}],
-                [{"power": 90, "duration": "12:00"}, {"power": 60, "duration": "4:00"}],
-                [{"power": 90, "duration": "12:00"}, {"power": 60, "duration": "4:00"}],
-                [{"power": 50, "duration": "2:00"}],
-            ],
-        }
-
-        self.assertDictEqual(config, expected_config)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    expected_config = {
+        "name": "Test",
+        "settings": {
+            "sports_type": "cycling",
+            "ftp": 250,
+        },
+        "steps": [
+            [{"power": 50, "duration": "2:00"}],
+            [{"power": 90, "duration": "12:00"}, {"power": 60, "duration": "4:00"}],
+            [{"power": 90, "duration": "12:00"}, {"power": 60, "duration": "4:00"}],
+            [{"power": 50, "duration": "2:00"}],
+        ],
+    }
+    assert config == expected_config
